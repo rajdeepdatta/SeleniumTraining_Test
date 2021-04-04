79750e125519e271b19c45288d73d6aa1b717434
rajdeepdatta <40378240+rajdeepdatta@users.noreply.github.com>
2021-04-04 12:27:05 +0530

    Add files via upload

---
+++ AutomationTest/src/test/java/SeleniumTesting/Add.java
@@ -0,0 +1,19 @@
+package Automation;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String A = "Rajdeep";
+		String B = "Datta";
+	
+			
+		System.out.println(A);
+		System.out.println(B);
+		System.out.println("My name is : " + A +" "+ B);
+		
+		
+	}
+
+}
